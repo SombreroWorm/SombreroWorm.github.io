47044b9d6bc90e34c6a99b3c945d7c36cbed06aa
KingToot14 <armorgamesdot@gmail.com>
2021-08-25 01:53:32 -0400

    Update 404.js

--- javascript/404.js
+++ javascript/404.js
@@ -2,10 +2,12 @@ function Check404() {
     alert(window.location.href);
     var actual404 = false;
 
-    var urlPrefix = "sombreroworm.github.io/view/";
+    var urlPrefix = "https://sombreroworm.github.io/view/";
     var urlCode = window.location.href;
 
     urlCode = urlCode.substring(urlPrefix.length - 1);
+
+    alert(urlCode);
 }
 
 document.addEventListener('DOMContentLoaded', Check404, false);
