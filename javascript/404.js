75ce982335bb1c81517bbf5d83c9e51807546dc8
KingToot14 <armorgamesdot@gmail.com>
2021-08-25 00:13:49 -0400

    edited 404

---
+++ javascript/404.js
@@ -0,0 +1,11 @@
+function Check404() {
+    $.ajax({
+        type: 'HEAD',
+        url: window.location.href,
+    error: function() {
+            alert("Page " + window.location.href + " does not exist!");
+    }
+    });
+}
+
+document.addEventListener('DomContentLoaded', Check404, false);
