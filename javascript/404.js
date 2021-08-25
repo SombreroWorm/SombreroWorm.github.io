8b365766fad7bcda086a62930da09c64f1903895
KingToot14 <armorgamesdot@gmail.com>
2021-08-25 09:29:15 -0400

    Update 404.js

--- javascript/404.js
+++ javascript/404.js
@@ -2,6 +2,8 @@ var urlPrefix = "https://SombreroWorm.github.io/view/";
 var urlCode = window.location.href;
 var real404 = false;
 
+var version = 1.1;
+
 function Check404() {
     if (!urlCode.startsWith(urlPrefix)) {
         real404 = true;
@@ -9,7 +11,8 @@ function Check404() {
     
     urlCode = urlCode.substring(urlPrefix.length - 1);
 
-    alert("Url: " + window.location.href + "\n" + 
+    alert("Version: " + version + "\n" + 
+          "Url: " + window.location.href + "\n" + 
           "Trimmed: " + urlCode + "\n" + 
           "Length: " + urlPrefix.length);
 }
